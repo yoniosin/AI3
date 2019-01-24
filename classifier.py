7355c59ad8192571fb7050310dba96c9aabd3606
NaamaPearl <naama.pearl@gmail.com>
2019-01-25 00:07:31 +0200

    finish

--- classifier.py
+++ classifier.py
@@ -4,14 +4,20 @@ import numpy as np
 import heapq
 import random
 import pickle
-import sklearn
 import matplotlib.pyplot as plt
-from sklearn.decomposition import PCA
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import Perceptron
-from mpl_toolkits.mplot3d import Axes3D
 from functools import reduce
-import csv
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import svm
+
+
+class Normalizer:
+    def __init__(self, data):
+        self.mean = np.mean(data, axis=0)
+        self.var = np.var(data, axis=0)
+
+    def transform(self, data):
+        return (data - self.mean) / self.var ** 0.5
 
 
 def euclidain_distance(vec1, vec2, weights=None):
@@ -63,6 +69,78 @@ def normalize_data(data):
     return (data - np.mean(data, axis=0)) / var ** 0.5
 
 
+class SvmFactory(abstract_classifier_factory):
+    def __init__(self, kernel):
+        self.kernel = kernel
+
+    def train(self, data, labels, weights=None):
+        return SvmClassifier(self.kernel, data, labels, weights)
+
+
+class SvmClassifier(abstract_classifier):
+    def __init__(self, kernel, data, labels, weights):
+        self.normalizer = Normalizer(data)
+        self.svm = svm.SVC(kernel=kernel, gamma='scale')
+        self.svm.fit(self.normalizer.transform(data), labels)
+
+    def classify(self, features):
+        norm_features = self.normalizer.transform(features)
+        return self.svm.predict(norm_features)[0]
+
+
+class TreeKnnClassifier(abstract_classifier):
+    def __init__(self, criterion, depth, k, samples, labels):
+        self.tree = DecisionTreeClassifier(criterion=criterion, max_depth=depth)
+        self.tree.fit(X=samples, y=labels)
+
+        factory = knn_factory(k)
+        train_sets = self.getLeafIDs(samples, labels)
+        self.knn_classifiers = {leaf: factory.train(train_sets[leaf]['samples'], train_sets[leaf]['labels'],
+                                                    train_sets[leaf]['weights'])
+                                for leaf in train_sets.keys()}
+
+    def getLeafIDs(self, data, labels):
+        train_sets = {}
+        for sample, label in zip(data, labels):
+            leaf = self.getLeaf(sample)
+            try:
+                train_sets[leaf]['samples'].append(sample)
+                train_sets[leaf]['labels'].append(label)
+
+            except KeyError:
+                train_sets[leaf] = {'samples': [sample], 'labels': [label], 'weights': self.generateWeights(sample)}
+
+        return train_sets
+
+    def getLeaf(self, sample):
+        return self.tree.decision_path(sample.reshape(1, -1)).indices[-1]
+
+    def generateWeights(self, sample):
+        weights = np.ones(sample.shape)
+        for node in self.tree.decision_path(sample.reshape(1, -1)).indices[:-1]:
+            weights[self.tree.tree_.feature[node]] = 0
+
+        return weights
+
+    def classify(self, sample):
+        """
+        Finds relevant knn classifier, and returns it's result on the given sample.
+        :param sample:
+        :return:
+        """
+        return self.knn_classifiers[self.getLeaf(sample)].classify(sample)
+
+
+class TreeKnnFactory(abstract_classifier_factory):
+    def __init__(self, criteria, depth, k):
+        self.criteria = criteria
+        self.depth = depth
+        self.k = k
+
+    def train(self, samples, labels):
+        return TreeKnnClassifier(self.criteria, self.depth, self.k, samples, labels)
+
+
 class knn_classifier(abstract_classifier):
     def __init__(self, k, data, labels, weights):
         self.k = k
@@ -79,7 +157,6 @@ class knn_classifier(abstract_classifier):
         nearest = [heapq.heappop(dist_heap).label for _ in range(self.k)]
         label_count = Counter(nearest)
         return max(label_count, key=lambda m: label_count[m])
-
 
 class knn_factory(abstract_classifier_factory):
     def __init__(self, k):
@@ -103,8 +180,26 @@ class tree_classifier(abstract_classifier):
         self.tree.fit(X=data, y=labels)
 
     def classify(self, features):
-        return self.tree.predict(X=features)
+        return self.tree.predict(X=features)[0]
 
+class forest_factory(abstract_classifier_factory):
+    def train(self, data, labels):
+        return forest_clasiffier(data, labels)
+
+class forest_clasiffier(abstract_classifier):
+    def __init__(self, data, labels):
+        knn_fact_5 = knn_factory(5)
+        knn_fact_1 = knn_factory(1)
+        svm_factory = SvmFactory('rbf')
+        self.classifiers_list = [
+            knn_fact_1.train(data[0], labels[0]),
+            svm_factory.train(data[1], labels[1]),
+            knn_fact_5.train(data[2], labels[2])
+        ]
+
+    def classify(self, features):
+        res = [classifier.classify(features) for classifier in self.classifiers_list]
+        return max(set(res), key=res.count)
 
 class perceptron_factory(abstract_classifier_factory):
     def train(self, data, labels):
@@ -121,57 +216,86 @@ class perceptron_classifier(abstract_classifier):
         return self.perceptron.predict(self.normalizer.transform(features))
 
 
-def apply_PCA(data_set, n_components):
-    X = data_set[0]
-    Y = data_set[1]
+def evaluate(classifier_factory: abstract_classifier_factory, k, suffix='', component=None):
+    need_to_load_fourier = suffix == 'forest'
+    if not need_to_load_fourier:
+        test_folds = [load_k_fold_data(idx, suffix) for idx in range(k)]
+    else:
+        test_folds = [load_k_fold_data(idx, 'time') for idx in range(k)]
+        test_folds_fourier = [load_k_fold_data(idx, 'fourier') for idx in range(k)]
+        test_folds_scale = [load_k_fold_data(idx, 'scale') for idx in range(k)]
 
-    normalizer = Normalizer(X)
-    X_scale = normalizer.transform(X)
-
-    pca = PCA(n_components=n_components)
-    pca.fit(X_scale)
-    projected_X = pca.transform(X_scale)
-
-    # fig = plt.figure()
-    # ax = fig.add_subplot(111, projection='3d')
-    # ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=Y)
-
-    return projected_X, Y
-
-
-def evaluate(classifier_factory: abstract_classifier_factory, k, suffix=''):
-    test_folds = [load_k_fold_data(idx, suffix) for idx in range(k)]
     N = 0
     accuracy = 0
     error = 0
+    precision_vec = [0, 0]
+    recall_vec = [0, 0]
 
     for fold in range(k):
         train_data, train_labels = reduce(lambda a, b: (np.concatenate([a[0], b[0]]), np.concatenate([a[1], b[1]])),
                                           test_folds[1:])
-        classifier: abstract_classifier = classifier_factory.train(train_data, train_labels)
         val_data, val_labels = test_folds[0]
-        evaluate_tree(classifier.tree, val_data[0].reshape(-1, 1))
+
+        if need_to_load_fourier:
+            train_data_fourier, train_labels_fourier = reduce(lambda a, b: (np.concatenate([a[0], b[0]]), np.concatenate([a[1], b[1]])),
+                                              test_folds_fourier[1:])
+            val_data_fourier, val_labels_fourier = test_folds_fourier[0]
+            train_data_scale, train_labels_scale = reduce(
+                lambda a, b: (np.concatenate([a[0], b[0]]), np.concatenate([a[1], b[1]])),
+                test_folds_scale[1:])
+            val_data_scale, val_labels_scale= test_folds_scale[0]
+
+        # if it is fourier data, apply LPF / BPF (window)
+        if (suffix in ['fourier', 'pca'] or need_to_load_fourier) and component is not None:
+            train_data = train_data[:, component[0]:component[1]]
+            val_data = val_data[:, component[0]:component[1]]
+            if need_to_load_fourier:
+                train_data_fourier = train_data_fourier[:, component[0]:component[1]]
+                val_data_fourier = val_data_fourier[:, component[0]:component[1]]
+
+                train_data_scale= train_data_scale[:, component[0]:component[1]]
+                val_data_scale = val_data_scale[:, component[0]:component[1]]
+
+                train_data = [train_data, train_data_fourier, train_data_scale]
+                train_labels = [train_labels, train_labels_fourier, train_labels_scale]
+
+        classifier: abstract_classifier = classifier_factory.train(train_data, train_labels)
         N += len(val_labels)
-        right_decision_path = []
-        wrong_decision_path = []
+        error_dict = {True: 0, False: 0}
+        accuracy_dict = {True: 0, False: 0}
         for sample, label in zip(val_data, val_labels):
-            if classifier.classify(sample.reshape(1, -1)) == label:
-                decision_path = right_decision_path
+            class_res = classifier.classify(sample.reshape(1, -1))
+            if class_res == label:
+                accuracy_dict[label] += 1
                 accuracy += 1
+                if class_res:
+                    precision_vec[0] += 1
+                    recall_vec[0] += 1
             else:
-                decision_path = wrong_decision_path
                 error += 1
-            decision_path.append(classifier.tree.decision_path(sample.reshape(1, -1)).indices)
+                error_dict[label] += 1
+
+            if class_res:
+                precision_vec[1] += 1
+            if label:
+                recall_vec[1] += 1
 
         test_folds.append(test_folds.pop(0))
+        if need_to_load_fourier:
+            test_folds_fourier.append(test_folds_fourier.pop(0))
+            test_folds_scale.append(test_folds_scale.pop(0))
 
+    precision = precision_vec[0] / precision_vec[1]
+    recall = recall_vec[0] / recall_vec[1]
+    f1 = 2 * (precision * recall) / (precision + recall)
     return accuracy / N, error / N
 
 
-def compare_k(k_vec, factory):
+def compare_k(k_vec, factory, suffix='', n_component=None):
     _res = {}
     for k_val in k_vec:
-        _res[k_val] = (evaluate(factory(k_val), 2))
+        print('evaluate' + str(k_val))
+        _res[k_val] = (evaluate(factory(k_val), 2, suffix, n_component))
 
     return _res
 
@@ -209,7 +333,6 @@ def section3():
 
 
 def section6():
-    data_set = load_data()
     k_list = [1, 3, 5, 7, 13]
     res = compare_k(k_list, knn_factory)
     print_csv(res, '6')
@@ -228,15 +351,8 @@ def section7():
     print_csv(res, '12')
 
 
-def part_c():
-    tree_classifier = tree_factory('entropy')
-    res = evaluate(tree_classifier, 2)
-    print('a')
-
-
 if __name__ == '__main__':
-    # section3()
-    # section6()
-    # section7()
-    part_c()
+    section3()
+    section6()
+    section7()
     print('a')
--- competition.py
+++ competition.py
@@ -1,67 +1,188 @@
 from hw3_utils import *
-from classifier import knn_factory, evaluate, apply_PCA, split_crosscheck_groups
-from sklearn.tree import DecisionTreeClassifier
+from classifier import *
+# import neurokit as nk
+from imblearn.over_sampling import SMOTE, ADASYN
+import scipy
+from sklearn.decomposition import PCA
 
 
-class TreeKnnClassifier(abstract_classifier):
-    def __init__(self, criterion, depth, k, samples, labels):
-        self.tree = DecisionTreeClassifier(criterion=criterion, max_depth=depth)
-        self.tree.fit(X=samples, y=labels)
+def remove_corrupted_data(data):
+    for i in [319, 519, 550, 908]:
+        data[0] = np.delete(data[0], (i), axis=0)
+        del data[1][i]
 
-        factory = knn_factory(k)
-        train_sets = self.getLeafIDs(samples, labels)
-        self.knn_classifiers = {leaf: factory.train(train_sets[leaf]['samples'], train_sets[leaf]['labels'],
-                                                    train_sets[leaf]['weights'])
-                                for leaf in train_sets.keys()}
-
-    def getLeafIDs(self, data, labels):
-        train_sets = {}
-        for sample, label in zip(data, labels):
-            leaf = self.getLeaf(sample)
-            try:
-                train_sets[leaf]['samples'].append(sample)
-                train_sets[leaf]['labels'].append(label)
-
-            except KeyError:
-                train_sets[leaf] = {'samples': [sample], 'labels': [label], 'weights': self.generateWeights(sample)}
-
-        return train_sets
-
-    def getLeaf(self, sample):
-        return self.tree.decision_path(sample.reshape(1, -1)).indices[-1]
-
-    def generateWeights(self, sample):
-        weights = np.ones(sample.shape)
-        for node in self.tree.decision_path(sample.reshape(1, -1)).indices[:-1]:
-            weights[self.tree.tree_.feature[node]] = 0
-
-        return weights
-
-    def classify(self, sample):
-        """
-        Finds relevant knn classifier, and returns it's result on the given sample.
-        :param sample:
-        :return:
-        """
-        return self.knn_classifiers[self.getLeaf(sample)].classify(sample)
+    return data
 
 
-class TreeKnnFactory(abstract_classifier_factory):
-    def __init__(self, criteria, depth, k):
-        self.criteria = criteria
-        self.depth = depth
-        self.k = k
+def create_time(data, fold_num, sample_type):
+    # split_crosscheck_groups(data, fold_num, 'time' + sample_type)
+    sample_len = data[0][0].shape[0]
+    t = np.arange(sample_len)
+    plot_random_samples(data, t)
 
-    def train(self, samples, labels):
-        return TreeKnnClassifier(self.criteria, self.depth, self.k, samples, labels)
 
+def create_fourier(data, fold_num, sample_type):
+    X = data[0]
+    # X = (X.T - X.mean(axis=1)).T
+    X_fft = np.asarray([np.fft.fft(x) for x in X])
+    X_fft_shift = np.asarray([np.fft.fftshift(x) for x in X_fft])
+    data_fourier = [np.abs(X_fft), data[1]]
+    data_shift = [np.abs(X_fft_shift), data[1]]
+
+    sample_len = data[0][0].shape[0]
+    f = np.linspace(-np.pi, np.pi, sample_len)
+    plot_random_samples(data_shift, f)
+    # split_crosscheck_groups(data_fourier, fold_num, 'fourier' + sample_type)
+
+
+def create_scale(data, fold_num, sample_type):
+    sample_len = data[0][0].shape[0]
+
+    # scale train data
+    X = data[0]
+    X_scale = []
+    for i, x in enumerate(X):
+        x_crop = np.asarray(list(filter(lambda v: abs(v) > 0.05, x))).reshape(-1, 1)
+        x_scale = scipy.misc.imresize(x_crop, (sample_len, 1)).reshape(-1) / 255
+        X_scale.append(x_scale)
+    X_scale = np.asarray(X_scale)
+
+
+    # scale test data
+    test = data[2]
+    test_scale = []
+    for i, x in enumerate(test):
+        test_crop = np.asarray(list(filter(lambda v: abs(v) > 0.05, x))).reshape(-1, 1)
+        scale = scipy.misc.imresize(test_crop, (sample_len, 1)).reshape(-1) / 255
+        test_scale.append(scale)
+    test_scale = np.asarray(test_scale)
+
+
+    data_scale = [X_scale, data[1], test_scale]
+    t = np.arange(sample_len)
+    plot_random_samples(data_scale, t)
+    # split_crosscheck_groups(data_scale, 2, 'scale' + sample_type)
+
+    return data_scale
+
+def prepare_data(fold_num):
+    org_data = list(load_data())
+    X_resampled, y_resampled = SMOTE().fit_resample(org_data[0], org_data[1])
+    print(sorted(Counter(y_resampled).items()))
+    data_resampled = [X_resampled, y_resampled]
+
+    for data, sample_type in zip([org_data], ['']):
+        # remove corrupted data
+        data = remove_corrupted_data(data)
+
+        # time
+        create_time(data, fold_num, sample_type)
+
+        # fourier
+        create_fourier(data, fold_num, sample_type)
+
+        # scale
+        create_scale(data, fold_num, sample_type)
+
+        # pca
+        create_PCA(data, fold_num, sample_type)
+
+def create_PCA(data_set, fold_num, sample_type):
+
+    X = data_set[0]
+    Y = data_set[1]
+
+    normalizer = Normalizer(X)
+    X_scale = normalizer.transform(X)
+
+    pca = PCA()
+    pca.fit(X_scale)
+    projected_X = pca.transform(X_scale)
+    pca_data = [projected_X, Y]
+    # split_crosscheck_groups(pca_data, fold_num, 'pca' + sample_type)
+
+def plot_random_samples(_data, x):
+    cdict = {True: (0.2, 0.5, 0.8), False: (0.8, 0.5, 0.2)}
+    fig, ax = plt.subplots()
+    for g in np.unique(_data[1]):
+        ix = np.where(_data[1] == g)
+        [ax.plot(x, (_data[0][tot_i]).T, c=cdict[g], label=(g if i == 0 else "")) for i, tot_i in
+         enumerate(ix[0][10:15])]
+    ax.legend()
+    ax.set_title('Random Samples of the Data')
+    plt.show()
+
+
+# classifiers_list = ['svm']
+classifiers_list = ['knn', 'tree', 'svm', 'perceptron', 'knn_tree']
+k_list = [1, 3, 5, 7, 13]
+
+factory = {'tree': tree_factory('entropy'),
+           'perceptron': perceptron_factory(),
+           'svm': SvmFactory('rbf'),
+           'forest': forest_factory(),
+           'knn_tree': TreeKnnFactory('entropy', 2, 1)
+           }
 
 if __name__ == '__main__':
-    # k_list = [1, 3, 5, 7, 13]
-    # res = compare_k(k_list, TreeKnnFactory)
-    # data = load_data()
-    # x_PCA = apply_PCA(data, n_components=100)
-    # split_crosscheck_groups(x_PCA, 2, 'competition')
-    tree_knn_factory = TreeKnnFactory('entropy', 1, 5)
-    res = evaluate(tree_knn_factory, 2, 'competition')
-    print('a')
+
+    # train the competition classifier
+    competition_factory = knn_factory(5)
+    data = list(load_data())
+    filtered_data = remove_corrupted_data(data)
+    scaled_data = create_scale(filtered_data, 1, 'Competition')
+    competition_classifier = competition_factory.train(scaled_data[0], scaled_data[1])
+
+    results = [competition_classifier.classify(feature) for feature in scaled_data[2]]
+    write_prediction(results)
+    '''
+    fold_num = 2
+    # prepare_data(fold_num)
+    dict_res = {}
+
+    # forest
+    fourier_component = [0, 30]
+    res_forest = evaluate(factory['forest'], fold_num, 'forest', fourier_component)
+    dict_res['forest'] = res_forest
+
+    # fourier analyze
+    method = 'fourier'
+    dict_res[method] = {}
+    fourier_component_list = [(0, 10), (0, 30), (0, 50), (1, 11), (1, 31), (1, 51)]
+    for classifier in classifiers_list:
+        dict_res[method][classifier] = {}
+        for component in fourier_component_list:
+            if classifier == 'knn':
+                res_knn = compare_k(k_list, knn_factory, method, component)
+                dict_res[method]['knn'][component] = res_knn
+            else:
+                res = evaluate(factory[classifier], fold_num, method, component)
+                dict_res[method][classifier][component] = res
+
+    # pca analyze
+    method = 'pca'
+    dict_res[method] = {}
+    pca_component_list = [(0, 187), (0, 150), (0, 100), (0, 70), (0, 50), (0, 30), (0, 15)]
+    for classifier in classifiers_list:
+        dict_res[method][classifier] = {}
+        for component in pca_component_list:
+            if classifier == 'knn':
+                res_knn = compare_k(k_list, knn_factory, method, component)
+                dict_res[method]['knn'][component] = res_knn
+            else:
+                res = evaluate(factory[classifier], fold_num, method, component)
+                dict_res[method][classifier][component] = res
+
+    for method in ['scale', 'time']:
+        dict_res[method] = {}
+        for classifier in classifiers_list:
+            dict_res[method][classifier] = {}
+            if classifier == 'knn':
+                res_knn = compare_k(k_list, knn_factory, method)
+                dict_res[method]['knn'] = res_knn
+            else:
+                res = evaluate(factory[classifier], fold_num, method)
+                dict_res[method][classifier] = res
+    '''
+
+    print('done')
